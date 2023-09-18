563f0b76d8b9f1bec4db4e131150742dee500667
Alfred Premkumar <alfredrpk@gmail.com>
2023-09-17 22:51:33 -0400

    some files i forgot lol i promise im good at git

---
+++ next.config.js
@@ -0,0 +1,2 @@
+/** @type {import('next').NextConfig} */
+module.exports = { images: { domains: ['i.imgur.com'], formats: ['image/avif', 'image/webp'], }, }
---
+++ posts/first.md
@@ -0,0 +1,8 @@
+---
+title: 'first post'
+date: '2023-09-17'
+---
+
+first first first
+
+more to come when i decide to write more, check back later
